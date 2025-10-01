33dc8887d562e44caf8530e803c73f80595bc5eb
Himanshu Singh <hk39435@gmail.com>
2025-10-01 23:05:27 +0530

    First commit

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [react()],
   server: { 
     port: 5174
   },
   
-  // *** Add this 'base' property for GitHub Pages deployment ***
-  base: "/doctor_appointment/", 
+ 
 })
